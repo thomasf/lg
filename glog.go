266dd3484a8ac01a873a502828e95e12900dd740
Thomas Frössman <thomasf@jossystem.se>
2016-08-31 17:59:39 +0200

    Update godoc w. fork changes

--- glog.go
+++ glog.go
@@ -1,19 +1,17 @@
-// Go support for leveled logs, analogous to https://code.google.com/p/google-glog/
+// Go support for leveled logs
+//
+// This is a fork of https://github.com/golang/glog with changes:
+//
+// • Added support for colored levels/package highlighting.
+//
+// • Added simple in memory logging.
+//
+// • Added package pkg/promethues which has a prometheus line numbers per level metric counters.
+//
+// • Added support package pkg/lgexpire for expiering old lg/glog logs.
 //
 // Copyright 2013 Google Inc. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // Package lg implements logging analogous to the Google-internal C++ INFO/ERROR/V setup.
 // It provides functions Info, Warning, Error, Fatal, plus formatting variants such as
 // Infof. It also provides V-style logging controlled by the -v and -vmodule=file=2 flags.
